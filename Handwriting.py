9b24c1e3b2458e9004783756ab62b1d8f5bd40dc
caracena <claudio.aracena2@gmail.com>
2016-05-26 21:53:50 +1000

    update display images

--- Handwriting.py
+++ Handwriting.py
@@ -7,42 +7,44 @@ class HandwritingPredictor:
 
     def getNumberFromList(self,list):
         number = np.where(list == 1)[0][0]
-        return number;
+        return number
 
-    def displayNumber(self,numberArray,numberArr):
-        # Transforms array of 10 numbers into actual number
-        actualNumber = self.getNumberFromList(numberArr)
-        # Shows actual number
-        print(actualNumber)
+    def displayNumbers(self):
 
-        # Building image array with 16x16 pixels
-        #.
-        i = 16
-        k = 0
-        img = np.array(numberArray[:16])
-        #Each 16 elements corresponds to one line of the 16 x 16 matrix
-        while i <= len(numberArray):
+        # create matrix for image
+        for j in range(10):
+            index = self.y_labels.index(j)
+            img = np.reshape(self.X.ix[index, :].values, (16, 16))
+            img = np.concatenate((np.zeros((16, 2)), img), axis=1)
+            count = 0
+            for i in range(index+1,self.X.shape[1]):
+                temp = np.reshape(self.X.ix[i, :].values, (16, 16))
+                img = np.concatenate((img, temp), axis=1)
+                img = np.concatenate((img, np.zeros((16,2))), axis=1)
+                count += 1
+                if count == 10:
+                    if j == 0:
+                        image = img.copy()
+                        image = np.concatenate((np.zeros((2, image.shape[1])),image), axis=0)
+                    else:
+                        image = np.concatenate((image, img), axis=0)
+                        image = np.concatenate((image, np.zeros((2,image.shape[1]))), axis=0)
+                    break
 
-            temp = np.array(numberArray[k:i])
-            img = np.vstack((img,temp))
-
-            k = i
-            i += 16
 
         # Plot image
-        plt.imshow(img,cmap=plt.cm.gray_r,interpolation="nearest")
+        plt.imshow(image,cmap=plt.cm.gray_r,interpolation="nearest")
         plt.show()
 
     def loadFiles(self,name):
         # Loading Files
         allData = pd.read_csv(name,header=None,sep=" ")
-        X = allData.ix[:,:255]
-        y = allData.ix[:,256:265]
-        y_labels = [(row[row == 1].index[0] - 256) for index, row in y.iterrows()]
-        return X, y_labels, y
+        self.X = allData.ix[:,:255]
+        self.y = allData.ix[:,256:265]
+        self.y_labels = [(row[row == 1].index[0] - 256) for index, row in self.y.iterrows()]
 
-    def predict(self,classifier,X,y,folds=10):
+    def predict(self,classifier,folds=10):
         #Run a cross validation on the give classifier.
-        scores = cross_val_score(classifier, X, y, cv=folds, scoring="accuracy");
+        scores = cross_val_score(classifier, self.X, self.y_labels, cv=folds, scoring="accuracy");
         return scores.mean()
 
--- Main.py
+++ Main.py
@@ -7,12 +7,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 # Create a classifier and feed into the predict function. Cross validation will be automatically used
 def main():
     predictor = HandwritingPredictor()
-    X, y_labels, y = predictor.loadFiles("semeion.data")
+    predictor.loadFiles("semeion.data")
+    predictor.displayNumbers()
 
     models = [svm.SVC(gamma=0.01, C=100),
               RandomForestClassifier(n_estimators=500, max_features='auto', n_jobs = -1),
@@ -22,11 +22,10 @@ def main():
               GaussianNB(),
               LogisticRegression(solver='lbfgs', multi_class = 'ovr', max_iter = 50 ,n_jobs = -1),
               KNeighborsClassifier(3, n_jobs = -1),
-              LinearDiscriminantAnalysis(),
-              QuadraticDiscriminantAnalysis()]
+              LinearDiscriminantAnalysis()]
 
     for model in models:
-        print('accuracy for model {}: {}'.format(model.__str__,predictor.predict(model, X, y_labels)))
+        print('accuracy for model {}: {}'.format(model.__str__,predictor.predict(model)))
 
 if __name__ == "__main__":
     main()
